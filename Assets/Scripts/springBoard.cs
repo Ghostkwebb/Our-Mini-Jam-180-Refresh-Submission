520d5b4aa46f7ebb8b52127f9b786478bbbd069e
Sharad Mayank Sinha <ghostkwebb@gmail.com>
2025-03-16 19:55:07 +0530

    springBoard done with full functionality

--- Assets/Scripts/springBoard.cs
+++ Assets/Scripts/springBoard.cs
@@ -1,28 +1,45 @@
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 
 public class springBoard : MonoBehaviour
 {
-    [SerializeField] private GameObject gameObject;
-    [SerializeField] private float rotateAngle;
-    [SerializeField] private Transform rb;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private float rotateAngle = 30f;
+    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float launchForce = 10f;
+    private bool isRotating = false;
+    private float currentRotation = 0f;
+    private float targetRotation;
+    private Rigidbody2D ballRb;
+    private float initialZRotation;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ball")
+        if (collision.gameObject.CompareTag("Ball") && !isRotating)
         {
-            transform.Rotate(0, 0, rotateAngle);
+            isRotating = true;
+            ballRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (ballRb != null)
+            {
+                Vector2 launchDirection = transform.up;
+                ballRb.AddForce(launchDirection * launchForce, ForceMode2D.Impulse);
+                initialZRotation = transform.localEulerAngles.z;
+                targetRotation = initialZRotation + rotateAngle;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRotating)
+        {
+            float step = rotationSpeed * Time.fixedDeltaTime;
+            currentRotation = Mathf.MoveTowards(transform.localEulerAngles.z, targetRotation, step);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, currentRotation);
+
+            if (Mathf.Abs(currentRotation - targetRotation) < 0.1f)
+            {
+                isRotating = false;
+            }
         }
     }
 }
